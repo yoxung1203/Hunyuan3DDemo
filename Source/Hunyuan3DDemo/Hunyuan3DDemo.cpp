6998de7612a27114887852b3119cda8cec385bf3
yoxung1203 <1143969393@qq.com>
2025-07-04 15:57:04 +0800

    init

---
+++ Source/Hunyuan3DDemo/Hunyuan3DDemo.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Hunyuan3DDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Hunyuan3DDemo, "Hunyuan3DDemo" );
